fc9a4af189f847054f64531c699e18d4518e75a5
1natsu172 <1natsu172@users.noreply.github.com>
2020-06-01 03:35:58 +0900

    fix: eslint configurations

--- src/libs/getProjectCard.ts
+++ src/libs/getProjectCard.ts
@@ -1,7 +1,7 @@
 import {context} from '@actions/github'
-import {WebhookPayloadProjectCardProjectCard} from '@octokit/webhooks'
+import {Webhooks} from '@octokit/webhooks'
 
 export function getProjectCardNodeId(): string | undefined {
-  return (context.payload?.project_card as WebhookPayloadProjectCardProjectCard)
-    ?.node_id
+  return (context.payload
+    ?.project_card as Webhooks.WebhookPayloadProjectCardProjectCard)?.node_id
 }
