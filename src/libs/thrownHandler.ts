c58a7e82dc768477b56b131ce8cf952448631f22
1natsu172 <1natsu172@users.noreply.github.com>
2020-06-19 03:02:18 +0900

    chore: optional access

--- src/libs/thrownHandler.ts
+++ src/libs/thrownHandler.ts
@@ -3,7 +3,7 @@ import {isSkipAction, prettyStringify} from '../utils'
 
 export function thrownHandler(error: Error): void {
   core_error(prettyStringify(error))
-  if (isSkipAction(error.message)) {
+  if (isSkipAction(error?.message)) {
     info(error.message)
     process.exitCode = ExitCode.Success
   } else {
